a38fb277c542cb3b1c71668ce8a1685d8e0c7166
bilkees <bilkeeskv@gmail.com>
2022-01-17 12:52:15 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './service/api.service';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  { path: 'create-employee', component: EmployeeCreateComponent },
+  { path: 'edit-employee/:id', component: EmployeeEditComponent },
+  { path: 'employees-list', component: EmployeeListComponent }  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
